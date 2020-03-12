193098136ccbab57dbf73b66d37a6085118f3446
Axel Rammon <axelrammon48@gmail.com>
2020-03-12 09:58:12 -0300

    Updates

---
+++ precache-manifest.33d16d1998f83be1d377ec2cfc98d2df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c34396480cfb448faba2f16593d7b11",
+    "url": "/artigo-github-pages/index.html"
+  },
+  {
+    "revision": "21b3f4c071bb4847f5da",
+    "url": "/artigo-github-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d592016fba87651078d2",
+    "url": "/artigo-github-pages/static/js/2.01cbbbdc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/artigo-github-pages/static/js/2.01cbbbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b3f4c071bb4847f5da",
+    "url": "/artigo-github-pages/static/js/main.07b20326.chunk.js"
+  },
+  {
+    "revision": "4bd2c6ce5d2e460ddfaa",
+    "url": "/artigo-github-pages/static/js/runtime-main.06fe799e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artigo-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
